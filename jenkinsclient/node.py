483136cb06254655ec0d5e99b3291aa48e585c9a
TangMing <hummerstudio@163.com>
2020-04-15 21:37:33 +0800

    增加查看单个节点信息，查看节点列表功能

--- jenkinsclient/node.py
+++ jenkinsclient/node.py
@@ -15,10 +15,20 @@ class Node(object):
         for key in nodes.keys():
             node = nodes[key]
             if node.name == 'master':
-                print(node)
-            else:
-                print(node,
-                      node.get_architecture(),
-                      node.get_available_physical_memory(),
-                      node.get_available_swap_space(),
-                      node.get_labels())
+                node.name = '(master)'
+            # TODO: 后面三个数据的获取需要先修复jenkinsapi的bug
+            print(node,
+                  node.get_architecture(),
+                  node.get_available_swap_space(),
+                  node.get_temp_size(),
+                  node.get_labels())
+
+    def info(self, node_name):
+        """
+        查看节点信息
+        """
+        server = jenkins_server.get_jenkins_server()
+        if node_name == 'master':
+            node_name = '(master)'
+        info = server.get_node_info(node_name)
+        return info
