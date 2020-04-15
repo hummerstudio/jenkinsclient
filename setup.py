a05eed0dc67443766406116f1390ec3c4296facd
TangMing <hummerstudio@163.com>
2020-04-15 21:39:30 +0800

    update version for release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme:
 
 setuptools.setup(
     name='jenkinsclient',
-    version='0.5.0',
+    version='0.6.0',
     author="TangMing",
     author_email="hummerstudio@163.com",
     description="A powerful cross-platform Jenkins command-line client which supports multiple instances of Jennkins.",
