114701ec909ee8acbfd7ed44efe2be26bb28b0fc
TangMing <hummerstudio@163.com>
2020-05-16 13:48:20 +0800

    commands group queue support localized languages

--- jenkinsclient/queue.py
+++ jenkinsclient/queue.py
@@ -7,18 +7,15 @@ You may obtain a copy of Mulan PSL v2 at:
 THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 See the Mulan PSL v2 for more details.
 """
+import locale
+
 from jenkinsclient import jenkins_server
 
 
 class Queue(object):
-    """
-    Jenkins队列相关操作
-    """
+    """Manage Jenkins queue"""
     def cancle(self, queue_id):
-        """
-        取消队列中任务
-        :return:
-        """
+        """Cancel a task in queue"""
         server = jenkins_server.get_jenkins_server()
         try:
             server.cancel_queue(queue_id)
@@ -27,12 +24,15 @@ class Queue(object):
         return '取消队列中任务%d成功' % queue_id
 
     def ls(self):
-        """
-        查看队列
-        :return:
-        """
+        """List tasks in queue"""
         server = jenkins_server.get_jenkins_server()
         queue = server.get_queue_info()
         print('任务ID\t%s\t原因' % '任务链接'.ljust(50))
         for q in queue:
             print('%d\t%s\t%s' % (q['id'], q['task']['url'].ljust(50), q['why']))
+
+
+if locale.getlocale().__str__() == "('zh_CN', 'UTF-8')":
+    Queue.__doc__ = '管理Jenkins队列'
+    Queue.cancle.__doc__ = '取消队列中任务'
+    Queue.ls.__doc__ = '查看队列'
