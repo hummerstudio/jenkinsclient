63155b1d95265cf76a921a31a2f63a5ddb37c904
TangMing <hummerstudio@163.com>
2020-05-16 13:23:39 +0800

    commands group job support localized languages

--- jenkinsclient/job.py
+++ jenkinsclient/job.py
@@ -7,28 +7,23 @@ You may obtain a copy of Mulan PSL v2 at:
 THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 See the Mulan PSL v2 for more details.
 """
+import locale
+
 from jenkins import JenkinsException
 from jenkinsclient import jenkins_server
 
 
 class Job(object):
-    """
-    Jenkins任务相关操作
-    """
-
+    """Manage Jenkins jobs"""
     def build(self, job_name, parameters=None):
-        """
-        构建任务
-        """
+        """Build a job"""
         server = jenkins_server.get_jenkins_server()
         build_number = server.build_job(job_name, parameters=parameters)
         next_build_number = server.get_job_info(job_name)['nextBuildNumber']
         return '任务%s已加入队列，队列ID：%d，下次构建号：%d' % (job_name, build_number, next_build_number)
 
     def copy(self, from_name, to_name):
-        """
-        复制任务
-        """
+        """Copy a job"""
         server = jenkins_server.get_jenkins_server()
         try:
             server.copy_job(from_name, to_name)
@@ -37,9 +32,7 @@ class Job(object):
         return '复制任务%s到%s成功' % (from_name, to_name)
 
     def create(self, job_name, config_xml):
-        """
-        创建任务
-        """
+        """Create a job"""
         server = jenkins_server.get_jenkins_server()
         try:
             server.create_job(job_name, config_xml.strip())
@@ -48,9 +41,7 @@ class Job(object):
         return '创建任务%s成功' % job_name
 
     def delete(self, job_name):
-        """
-        删除任务
-        """
+        """Delete a job"""
         server = jenkins_server.get_jenkins_server()
         try:
             server.delete_job(job_name)
@@ -59,25 +50,19 @@ class Job(object):
         return '删除任务%s成功' % job_name
 
     def disable(self, job_name):
-        """
-        禁用任务
-        """
+        """Disable a job"""
         server = jenkins_server.get_jenkins_server()
         server.disable_job(job_name)
         return '禁用任务%s成功' % job_name
 
     def enable(self, job_name):
-        """
-        启用任务
-        """
+        """Enable a job"""
         server = jenkins_server.get_jenkins_server()
         server.enable_job(job_name)
         return '启用任务%s成功' % job_name
 
     def has(self, job_name):
-        """
-        查看任务是否已存在
-        """
+        """Whether has a job """
         server = jenkins_server.get_jenkins_server()
         exists = server.job_exists(job_name)
         if exists:
@@ -86,9 +71,7 @@ class Job(object):
             return '任务%s不存在' % job_name
 
     def info(self, job_name):
-        """
-        显示任务信息
-        """
+        """Display job information"""
         server = jenkins_server.get_jenkins_server()
         try:
             job_info = server.get_job_info(job_name)
@@ -97,9 +80,7 @@ class Job(object):
         return job_info
 
     def ls(self):
-        """
-        显示任务列表
-        """
+        """List jobs"""
         server = jenkins_server.get_jenkins_server()
         job_list_content = '任务名称'.ljust(26) + '类型'.ljust(28) + '链接\n'
         job_list_content += '--------'.ljust(30) + '--------'.ljust(30) + '--------\n'
@@ -109,9 +90,7 @@ class Job(object):
         return job_list_content
 
     def rename(self, from_name, to_name):
-        """
-        重命名任务
-        """
+        """Rename a job"""
         server = jenkins_server.get_jenkins_server()
         try:
             server.rename_job(from_name, to_name)
@@ -120,9 +99,7 @@ class Job(object):
         return '重命名%s为%s成功' % (from_name, to_name)
 
     def xml(self, job_name):
-        """
-        获取任务的config.xml内容
-        """
+        """Display content of job's config.xml file"""
         server = jenkins_server.get_jenkins_server()
         try:
             config_xml = server.get_job_config(job_name)
@@ -130,3 +107,17 @@ class Job(object):
             return '获取任务%s的config.xml内容失败：%s' % (job_name, str(e))
         return config_xml.replace(" ", "").replace("\n", "")
 
+
+if locale.getlocale().__str__() == "('zh_CN', 'UTF-8')":
+    Job.__doc__ = '管理Jenkins任务'
+    Job.build.__doc__ = '构建任务'
+    Job.copy.__doc__ = '复制任务'
+    Job.create.__doc__ = '创建任务'
+    Job.delete.__doc__ = '删除任务'
+    Job.disable.__doc__ = '禁用任务'
+    Job.enable.__doc__ = '启用任务'
+    Job.has.__doc__ = '查看任务是否已存在'
+    Job.info.__doc__ = '显示任务信息'
+    Job.ls.__doc__ = '显示任务列表'
+    Job.rename.__doc__ = '重命名任务'
+    Job.xml.__doc__ = '显示任务的config.xml内容'
