6145b59f2c69151a346a0f90c7232a747c2a215c
TangMing <hummerstudio@163.com>
2020-05-16 15:16:15 +0800

    update version for release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme:
 
 setuptools.setup(
     name='jenkinsclient',
-    version='1.5.0',
+    version='1.5.1',
     author="TangMing",
     author_email="hummerstudio@163.com",
     description="A powerful cross-platform Jenkins command line client which supports multiple instances of Jenkins.",
