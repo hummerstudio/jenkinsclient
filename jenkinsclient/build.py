e31c22977a29d1c10443b4e18265fe271f4f6e24
TangMing <hummerstudio@163.com>
2020-05-16 11:46:33 +0800

    commands group build support localized languages

--- jenkinsclient/build.py
+++ jenkinsclient/build.py
@@ -7,18 +7,16 @@ You may obtain a copy of Mulan PSL v2 at:
 THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 See the Mulan PSL v2 for more details.
 """
+import locale
+
 from jenkins import JenkinsException
 from jenkinsclient import jenkins_server
 
 
 class Build(object):
-    """
-    Jenkins构建相关操作
-    """
+    """Manage builds"""
     def env(self, job_name, build_number):
-        """
-        显示构建环境变量
-        """
+        """Display env vars of a build"""
         server = jenkins_server.get_jenkins_server()
         try:
             env = server.get_build_env_vars(job_name, build_number)
@@ -27,9 +25,7 @@ class Build(object):
         return env
 
     def log(self, job_name, build_number):
-        """
-        显示构建日志
-        """
+        """Display log of a build"""
         server = jenkins_server.get_jenkins_server()
         try:
             console_text = server.get_build_console_output(job_name, build_number)
@@ -38,9 +34,7 @@ class Build(object):
         return console_text
 
     def info(self, job_name, build_number):
-        """
-        显示构建信息
-        """
+        """Display info of a build"""
         server = jenkins_server.get_jenkins_server()
         try:
             build_info = server.get_build_info(job_name, build_number)
@@ -49,12 +43,18 @@ class Build(object):
         return build_info
 
     def report(self, job_name, build_number):
-        """
-        显示构建测试报告
-        """
+        """Display test report of a build"""
         server = jenkins_server.get_jenkins_server()
         try:
             report = server.get_build_test_report(job_name, build_number)
         except JenkinsException as e:
             return '%s#%d不存在' % (job_name, build_number)
         return report
+
+
+if locale.getlocale().__str__() == "('zh_CN', 'UTF-8')":
+    Build.__doc__ = 'Jenkins构建相关操作'
+    Build.env.__doc__ = '显示构建环境变量'
+    Build.log.__doc__ = '显示构建日志'
+    Build.info.__doc__ = '显示构建信息'
+    Build.report.__doc__ = '显示构建测试报告'
